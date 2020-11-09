df95cd56301810ce8d380a6e77600b8434a3a76c
hackchan <fabiorojas7@gmail.com>
2020-11-09 00:20:55 -0500

    styled-components

---
+++ src/App.js
@@ -0,0 +1,3 @@
+import React from 'React'
+
+export const App = () => <h1>App principal</h1>
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 import React from 'react'
 import ReactDom from 'react-dom'
-ReactDom.render(<h1>Hola React</h1>, document.getElementById('app'))
+import { App } from './App'
+ReactDom.render(<App />, document.getElementById('app'))
