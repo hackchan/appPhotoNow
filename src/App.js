b9c5942c59d43fa5ccb0eb617ce2f9820a481e72
hackchan <fabiorojas7@gmail.com>
2020-11-10 22:38:40 -0500

    se crea el logo codeNow

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from 'react'
 import { ListOfCategory } from './components/ListOfCategory'
 import { ListOfPhotoCard } from './components/ListOfPhotoCard'
 import { GlobalStyle } from './styles/GlobalStyle'
+import { Logo } from './components/Logo'
 export const App = () => (
   <>
+    <Logo />
     <GlobalStyle />
     <ListOfCategory />
     <ListOfPhotoCard />
